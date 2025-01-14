9d5bb5f8c4a2a8bc94f9625531cfbdd3a6d251df
Rohit Kumar <rk98991439@gmail.com>
2025-01-14 13:56:47 +0530

    learing day 1 Variables and DataTypes in JS

---
+++ Learn/variables-datatypes.js
@@ -0,0 +1,29 @@
+// Variables and DataTypes in JS
+// JavaScript has 8 Datatypes
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object
+
+// The Object Datatype
+// The object data type can contain both built-in objects, and user defined objects:
+
+// Built-in object types can be:
+
+// objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.
+
+
+let x = 10;
+var z = 11;
+console.log(x);
+x = x+z
+console.log(x);
+let c = "ew";
+console.log(c);
+
+c = c+x
+console.log(c);
