b8e60748b3427ced2456eadd9e78fd3fefd271df
Rohit Kumar <rk98991439@gmail.com>
2025-01-14 18:00:00 +0530

    learing day 1 Operators & Conditionals in JS

---
+++ Learn/operators-conditional.js
@@ -0,0 +1,63 @@
+// Operators & Conditionals
+
+// Operator	Description
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Remainder)
+// ++	Increment
+// --	Decrement
+
+// Assignment Operators
+// Operator	Example	Same As
+// =	x = y	x = y
+// +=	x += y	x = x + y
+// -=	x -= y	x = x - y
+// *=	x *= y	x = x * y
+// /=	x /= y	x = x / y
+// %=	x %= y	x = x % y
+// **=	x **= y	x = x ** y
+
+// Shift Assignment Operators
+// Operator	Example	Same As
+// <<=	x <<= y	x = x << y
+// >>=	x >>= y	x = x >> y
+// >>>=	x >>>= y	x = x >>> y
+
+// Bitwise Assignment Operators
+// Operator	Example	Same As
+// &=	x &= y	x = x & y
+// ^=	x ^= y	x = x ^ y
+// |=	x |= y	x = x | y
+
+// Logical Assignment Operators
+// Operator	Example	Same As
+// &&=	x &&= y	x = x && (x = y)
+// ||=	x ||= y	x = x || (x = y)
+// ??=	x ??= y	x = x ?? (x = y)
+
+let a = 10;
+let b = 22
+console.log(a);
+console.log(++a);
+
+console.log("new \n line");
+
+console.log("\n");
+
+console.log(a++);
+
+console.log(a);
+
+console.log(a < b);
+
+// -------------------------------------------------------------------------------------------------------------
+
+if (a>=9){
+    console.log(a,"is larger then 9")
+}else {
+    console.log(a,"is smaller then 9")
+
+}
