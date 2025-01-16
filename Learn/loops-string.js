dbc09a11fde6f6bce8c10825e3190ee02556de01
Rohit Kumar <rk98991439@gmail.com>
2025-01-16 22:57:38 +0530

    learing day 3 Loops and String in JS

--- Learn/loops-string.js
+++ Learn/loops-string.js
@@ -1,1 +1,22 @@
 // Loops and String
+
+// Different Kinds of Loops
+// JavaScript supports different kinds of loops:
+
+// for - loops through a block of code a number of times
+// for/in - loops through the properties of an object
+// for/of - loops through the values of an iterable object
+// while - loops through a block of code while a specified condition is true
+// do/while - also loops through a block of code while a specified condition is true
+let x = 10;
+
+for (let i = 0; i < x; i++) {
+    console.log (i) ;
+  }
+
+
+  
+  const fruits = ['Apple', 'Banana', 'Orange'];
+for (x in fruits) {
+  console.log(x);
+}
